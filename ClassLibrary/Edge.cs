386a7ba6e6e6ca3aeaf8e258f8aaa2affe10c023
Kirill <kkornilov55@gmail.com>
2026-03-26 23:15:36 +0300

    Добавьте файлы проекта.

---
+++ ClassLibrary/Edge.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary
+{
+    public class Edge
+    {
+        public string To;
+        public int Weight;
+
+        public Edge(string to, int weight)
+        {
+            To = to;
+            Weight = weight;
+        }
+    }
+}
