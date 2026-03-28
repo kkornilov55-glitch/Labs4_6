e8a3b885bdb19c3b1c7dfef67495e02d5b440681
Kirill <kkornilov55@gmail.com>
2026-03-28 21:59:21 +0300

    Добавил DFS

--- ClassLibrary/Graph.cs
+++ ClassLibrary/Graph.cs
@@ -3,38 +3,72 @@
     public class Graph
     {
         public Dictionary<string, List<Edge>> adjacencyList = new Dictionary<string, List<Edge>>();
+
         public Dictionary<string, string> parent = new Dictionary<string, string>();
         public Dictionary<string, Colour> colour = new Dictionary<string, Colour>();
+        public bool cycle = false;
         public enum Colour { White, Gray, Black }
+
         public void ReadGraph()
         {
             StreamReader F = new StreamReader("river_graph.txt");
 
             //Считываем матрицу
             string currentString;
-            string vertexStart, vertexFinish;
+            string from, to;
             int weight;
             while (true)
             {
                 currentString = F.ReadLine();
                 if (string.IsNullOrEmpty(currentString)) break;
 
-                vertexStart = currentString.Split()[0];
-                vertexFinish = currentString.Split()[1];
-                weight = int.Parse(currentString.Split()[2]);
+                from = currentString.Split()[0];
+                to = currentString.Split()[1];
+                weight = int.Parse(currentString.Split(';')[2]);
 
-                if (!adjacencyList.ContainsKey(vertexStart)) adjacencyList[vertexStart] = new List<Edge>();
-                else adjacencyList[vertexStart].Add(new Edge(vertexFinish, weight));
+                if (!adjacencyList.ContainsKey(from)) adjacencyList[from] = new List<Edge>();
+                else adjacencyList[from].Add(new Edge(to, weight));
             }
-            foreach (var edge in adjacencyList.Keys)
-            {
-                colour.Add(edge, Colour.White);
-            }
+
+            F.Close();
         }
-        public void DFS()
+        private void InitColours()
         {
-            
+            foreach(var V in adjacencyList.Keys)
+            {
+                colour[V] = Colour.White;
+            }
+            parent.Clear();
         }
+        public List<string> DFS(string startV)
+        {
+            InitColours();
+            parent[startV] = null;
 
+            var way = new List<string>();
+            DFS_Recursive(startV, way);
+
+            return way;
+        }
+        private void DFS_Recursive(string V, List<string> way)
+        {
+            colour[V] = Colour.Gray;
+            way.Add(V);
+
+            foreach (Edge E in adjacencyList[V])
+            {
+                string neighbor = E.To;
+
+                if (neighbor != null)
+                {
+                    if (colour[neighbor] == Colour.White)
+                    {
+                        parent[neighbor] = V;
+                        DFS_Recursive(neighbor, way);
+                    }
+                }
+            }
+            colour[V] = Colour.Black;
+        }
     }
 }
